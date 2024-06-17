fc73e99e005481b22b5c427ef109eab79b313f5a
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2024-06-17 14:09:58 +0200

    La til løp for å oppdatere kobling mellom krav og tema

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from tema import run_etl_tema
 from prioriterte_krav import run_etl_prioriterte_krav
 from sist_oppdatert import run_etl_sist_oppdatert
 from alerts import run_etl_alerts
@@ -7,6 +8,7 @@ from beskrivelser import run_etl_beskrivelser
 
 
 if __name__ == "__main__":
+    run_etl_tema() # kobling mellom krav, tema og regelverk
     run_etl_prioriterte_krav() # prioriterte_krav
     run_etl_sist_oppdatert() # sist oppdatert på dokument-nivå
     run_etl_alerts() # alerts
---
+++ src/tema.py
@@ -0,0 +1,40 @@
+import json
+from datetime import datetime
+
+import pandas as pd
+import pandas_gbq
+
+from google.cloud import bigquery
+
+def run_etl_tema():
+    # Kobling mellom krav og regelverk
+    df = pandas_gbq.read_gbq("SELECT * FROM `teamdatajegerne-prod-c8b1.metrics.raw_krav`", "teamdatajegerne-prod-c8b1")
+
+    # Pakker ut json
+    df["regelverk"] = df["data"].apply(lambda x: [item["lov"] for item in json.loads(x)["regelverk"]])
+    df["kravNummer"] = df["data"].apply(lambda x: json.loads(x)["kravNummer"])
+    df = df.explode("regelverk")
+
+    # duplikater fordi regelverk er på detaljert nivå
+    df = df[["kravNummer", "regelverk"]].drop_duplicates().copy()
+
+    # Må koble på tema og
+    df_2 = pandas_gbq.read_gbq("SELECT distinct code as regelverk, tema, underavdeling FROM `teamdatajegerne-prod-c8b1.metrics.raw_tema`", "teamdatajegerne-prod-c8b1")
+
+    #Og merge
+    df = df.merge(df_2, on="regelverk", how="left")
+
+    # Skrive til BigQuery
+    client = bigquery.Client(project="teamdatajegerne-prod-c8b1")
+
+
+    project = "teamdatajegerne-prod-c8b1"
+    dataset = "metrics"
+    table = "krav_tema"
+
+    table_id = f"{project}.{dataset}.{table}"
+    job_config = bigquery.job.LoadJobConfig(write_disposition="WRITE_APPEND")
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+
+    return None
+
