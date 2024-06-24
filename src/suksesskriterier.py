785a017d4d10f6b245c0be81a5af726ad91a51ec
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2024-06-24 14:23:32 +0200

    Tror dette funker

--- src/suksesskriterier.py
+++ src/suksesskriterier.py
@@ -158,7 +158,7 @@ def run_etl_suksesskriterier():
 
     df_merged["version"] = datetime.now()
 
-    df_merged["tag"] = None
+    df_merged["tag"] = []
 
     # Skrive til BigQuery
     client = bigquery.Client(project="teamdatajegerne-prod-c8b1")
