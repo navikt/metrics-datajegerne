1c392b6d9bd3aaa294d8432bb0d0b03255bcf46e
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2024-06-24 13:52:33 +0200

    Fikset så tema merges uten å tenke på kravversjon

--- src/suksesskriterier.py
+++ src/suksesskriterier.py
@@ -141,8 +141,8 @@ def run_etl_suksesskriterier():
     df["kravFerdigUtfylt"] = df["statusKriterium"].apply(lambda x: True if all([True if item in ["OPPFYLT", "IKKE_RELEVANT", "IKKE_OPPFYLT"] else False for item in x]) else False)
 
     # Merger besvarelsene med oversikt over relevante krav
-    df_merged = df[["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon", "kravOppfylt", "kravFerdigUtfylt", "suksesskriterieId", "statusKriterium", "begrunnelse", "lastUpdated"]].merge(df_relevante_krav[["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon", "tema", "aktivVersjon"]].drop_duplicates(), on=["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon"], how="outer")
-    df_merged = df_merged.merge(df_relevante_krav[["etterlevelseDokumentasjonId", "etterlevelseNummer", "avdeling"]].drop_duplicates(), on="etterlevelseDokumentasjonId", how="outer")
+    df_merged = df[["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon", "kravOppfylt", "kravFerdigUtfylt", "suksesskriterieId", "statusKriterium", "begrunnelse", "lastUpdated"]].merge(df_relevante_krav[["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon", "aktivVersjon"]].drop_duplicates(), on=["etterlevelseDokumentasjonId", "kravNummer", "kravVersjon"], how="outer")
+    df_merged = df_merged.merge(df_relevante_krav[["etterlevelseDokumentasjonId", "etterlevelseNummer", "avdeling", "tema"]].drop_duplicates(), on="etterlevelseDokumentasjonId", how="outer")
 
     # Er en del missing etter merge
     df_merged.loc[df_merged["kravOppfylt"].isnull(), "kravOppfylt"] = False
