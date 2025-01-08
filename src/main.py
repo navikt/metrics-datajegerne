1177a2722b938114fd769a65c45ad5e377e861d7
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2025-01-08 12:02:15 +0100

    La til logger i main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from stage_etterlevelsebesvarelse import run_etl_etterlevelsebesvarelse
 from stage_krav import run_etl_tema
 from stage_etterlevelsedokument import run_etl_dokumenter
@@ -10,23 +12,39 @@ from datasets import run_etl_datasett_varslinger, run_etl_datasett_beskrivelser,
 
 
 
+
 if __name__ == "__main__":
+    # Setter opp logging
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Jobben starter!")
 
     # Flytting og transformering av data fra kilde
     run_etl_etterlevelsebesvarelse() # Besvarelser fra etterlevere
+    logging.info("Besvarelser kjørt!")
     run_etl_dokumenter() # Dokumentegenskaper
+    logging.info("Dokumenter kjørt!")
     run_etl_tema() # krav fordelt på tema
+    logging.info("Krav og tema kjørt!")
     run_etl_spoersmaal_og_svar() # spørsmål og svar til kraveier
+    logging.info("Spørsmål og svar kjørt!")
     run_etl_relasjoner() # koblinger mellom dokumenter
+    logging.info("Relasjoner kjørt!")
     run_etl_tildelt_og_notater() # Viser hvilke krav som er tildelt til hvem og hvor notater er skrevet
+    logging.info("Tildelt og notater kjørt!")
 
 
     # Videre transformering i python siden vi ikke orker å gjøre alt i SQL: Dette blir egne tabeller som blir datasett på Markedsplassen
     run_etl_datasett_varslinger()
+    logging.info("Varslinger kjørt!")
     run_etl_datasett_beskrivelser()
+    logging.info("Beskrivelser kjørt!")
     run_etl_datasett_prioritertlist()
+    logging.info("Prioritert kjørt kjørt!")
     run_etl_datasett_gjenbruk()
+    logging.info("Gjenbruk kjørt!")
     run_etl_sist_oppdatert()
+    logging.info("Sist oppdatert kjørt!")
 
 
     """
@@ -43,6 +61,4 @@ if __name__ == "__main__":
     run_etl_duplicates() # for å sjekke duplikater i etterlevelse
     run_etl_mordokumenter() # for å følge med på hvor mange som har mordokumenter
     """
-
-    print("Jobben kjørt!")
 
